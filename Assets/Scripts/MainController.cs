8782c804b5d89ba43349ce278f3baf8035ea48b3
Helen Murphy <helen@Helens-MacBook-Pro.local>
2018-12-19 22:17:17 +0000

    update

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour {
+
+    private Rigidbody rb;
+    //public GameObject bulletPrefab;
+    //public Transform bulletSpawn;
+
+	void Start () {
+       
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 6.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        /*
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+        */
+	}
+    /*
+    void Fire()
+    {
+        var bullet = (GameObject)Instantiate 
+        (
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation
+        );
+
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 10;
+
+        Destroy(bullet, 5.0f);
+    }
+    */
+    
+}
---
+++ Assets/Scripts/MazeLoader.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MazeLoader : MonoBehaviour {
+	public int mazeRows, mazeColumns;
+	public GameObject wall;
+    public GameObject floor;
+	public float size = 2f; // Size of row or column
+
+	private MazeCell[,] mazeCells;
+
+	// Use this for initialization
+	void Start () {
+        // Called to creat maze grid
+		InitializeMaze ();
+
+		MazeAlgorithm ma = new HuntAndKillMazeAlgorithm (mazeCells);
+		ma.CreateMaze ();
+	}
+
+
+	private void InitializeMaze() {
+
+		mazeCells = new MazeCell[mazeRows,mazeColumns];
+
+        // Iterate through the rows and columns creating a grid
+		for (int r = 0; r < mazeRows; r++) {
+			for (int c = 0; c < mazeColumns; c++) {
+				mazeCells [r, c] = new MazeCell ();
+
+				// Placing floor
+				mazeCells [r, c] .floor = Instantiate (floor, new Vector3 (r*size, -(size/2f), c*size), Quaternion.identity) as GameObject;
+				mazeCells [r, c] .floor.transform.Rotate (Vector3.right, 90f);
+
+                // Only add west wall if it's the first column, as this is the outer wall
+				if (c == 0) {
+					mazeCells[r,c].westWall = Instantiate (wall, new Vector3 (r*size, 0, (c*size) - (size/2f)), Quaternion.identity) as GameObject;
+				}
+
+                // Add east wall for each side
+				mazeCells [r, c].eastWall = Instantiate (wall, new Vector3 (r*size, 0, (c*size) + (size/2f)), Quaternion.identity) as GameObject;
+
+                // If the row is the first row, add north wall as this is the outer wall
+				if (r == 0) {
+					mazeCells [r, c].northWall = Instantiate (wall, new Vector3 ((r*size) - (size/2f), 0, c*size), Quaternion.identity) as GameObject;
+					mazeCells [r, c].northWall.transform.Rotate (Vector3.up * 90f);
+				}
+
+                // Add south wall for each side
+				mazeCells[r,c].southWall = Instantiate (wall, new Vector3 ((r*size) + (size/2f), 0, c*size), Quaternion.identity) as GameObject;
+				mazeCells [r, c].southWall.transform.Rotate (Vector3.up * 90f);
+			}
+		}
+	}
+}
